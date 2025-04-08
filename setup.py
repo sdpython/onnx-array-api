3d507ef60d493764ab6991330d54f982c71fd3fd
xadupre <xadupre@microsoft.com>
2025-04-08 11:52:57 +0200

    fix version

--- setup.py
+++ setup.py
@@ -62,9 +62,10 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
